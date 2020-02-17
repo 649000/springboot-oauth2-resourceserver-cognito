9948f9d8d6d651cc604d8af25128c7ff98aa1c6a
Naz <1567159@zone1.scb.net>
2020-02-17 10:57:32 +0800

    Initial Commit

---
+++ src/main/java/com/app/warrantychecker/WarrantycheckerApplication.java
@@ -0,0 +1,13 @@
+package com.app.warrantychecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarrantycheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarrantycheckerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/warrantychecker/WarrantycheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.warrantychecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarrantycheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
