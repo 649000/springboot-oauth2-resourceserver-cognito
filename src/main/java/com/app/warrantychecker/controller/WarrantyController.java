425fc2dbcb9cf2adf124bc35ccdeee8cc8de2c9f
Naz <>
2020-02-17 20:41:01 +0800

    Added warranty controller

--- src/main/java/com/app/warrantychecker/controller/WarrantyController.java
+++ src/main/java/com/app/warrantychecker/controller/WarrantyController.java
@@ -1,4 +1,28 @@
 package com.app.warrantychecker.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class WarrantyController {
+
+    @GetMapping("/warranty/{id}")
+    public String findOne( @PathVariable("id") String id) {
+        return "FindOne Foo with id=" + id;
+    }
+
+    @GetMapping("/warranty")
+    public String findAll() {
+        return "findAll";
+    }
+
+    @PostMapping("/warranty/2")
+    public String create(){
+        return "Warranty: Post Mapping";
+    }
+
+    @PatchMapping("/warranty/2")
+    public String update(){
+        return "PATCH";
+    }
+
 }
