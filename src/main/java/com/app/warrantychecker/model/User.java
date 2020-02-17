2bdcba9c3baed87c1ee17f66a596183ea6e3d619
Naz <>
2020-02-17 19:44:45 +0800

    Added postgreSQL and Deployed to Heroku

--- src/main/java/com/app/warrantychecker/model/User.java
+++ src/main/java/com/app/warrantychecker/model/User.java
@@ -3,7 +3,14 @@ package com.app.warrantychecker.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Setter
 @Getter
+@Entity
 public class User {
+
+    @Id
+    private String email;
 }
