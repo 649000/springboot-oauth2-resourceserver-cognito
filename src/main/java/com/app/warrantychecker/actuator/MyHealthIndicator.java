19be0010f91050d3461ddd6e31d72a47bb247494
Naz <>
2020-03-08 17:04:16 +0800

    Added Actuator properties and files

---
+++ src/main/java/com/app/warrantychecker/actuator/MyHealthIndicator.java
@@ -0,0 +1,37 @@
+package com.app.warrantychecker.actuator;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class MyHealthIndicator implements HealthIndicator {
+
+    // Docs: https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#writing-custom-healthindicators
+    // The identifier is the name of the bean without the HealthIndicator suffix.
+    // In this example, the health information is available in an entry named 'my'.
+
+    @Override
+    public Health health() {
+        int errorCode = check(); // perform some specific health check
+
+        // There are 4 status types:
+        // Up: Http 200
+        // Down: Http 503
+        // Unknown Http 200
+        // Out of Service: Http 503
+
+        if (errorCode != 0) {
+            return Health.down().withDetail("Error Code", errorCode).build();
+        }
+        return Health.up().build();
+    }
+
+    private int check(){
+        Random random = new Random();
+        return random.nextInt(1);
+    }
+
+}
