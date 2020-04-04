637f9ff276db59d1b027b8562b3b42ebe5b88dee
Naz <>
2020-04-04 22:40:01 +0800

    Added @Transactional annotation to services

--- src/main/java/com/app/warrantychecker/service/UserService.java
+++ src/main/java/com/app/warrantychecker/service/UserService.java
@@ -6,10 +6,14 @@ import com.app.warrantychecker.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+// It will rollback if there's a SQL exception
+@Transactional
 public class UserService {
 
     @Autowired
