437fedea9b702684dbe4df544364d756aa21ec57
Nazri <3991628+649000@users.noreply.github.com>
2021-05-15 21:42:11 +0800

    Test Commit

--- src/main/java/com/app/warrantychecker/WarrantycheckerApplication.java
+++ src/main/java/com/app/warrantychecker/WarrantycheckerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class WarrantycheckerApplication {
 
     public static void main(String[] args) {
+        //Comment
         SpringApplication.run(WarrantycheckerApplication.class, args);
 
     }
