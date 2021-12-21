e131ebf3e92c7afa2aebb4c94f180d9d763a3877
Nazri <649000@users.noreply.github.com>
2021-12-21 11:35:29 +0800

    Added Mock model/controller

---
+++ src/main/java/com/app/warrantychecker/controller/FooController.java
@@ -0,0 +1,42 @@
+package com.app.warrantychecker.controller;
+
+import com.app.warrantychecker.model.Foo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.apache.commons.lang3.RandomStringUtils.randomNumeric;
+
+@RestController
+@RequestMapping(value = "/foos")
+@Slf4j
+public class FooController {
+
+    @GetMapping(value = "/{id}")
+    public Foo findOne(@PathVariable Long id) {
+        return new Foo(Long.parseLong(randomNumeric(2)), randomAlphabetic(4));
+    }
+
+    @GetMapping
+    public List findAll(@AuthenticationPrincipal Jwt jwt) {
+        // To retrieve JWT claims
+//        log.info(jwt.getClaims().toString());
+        List fooList = new ArrayList();
+        fooList.add(new Foo(Long.parseLong(randomNumeric(2)), randomAlphabetic(4)));
+        fooList.add(new Foo(Long.parseLong(randomNumeric(2)), randomAlphabetic(4)));
+        fooList.add(new Foo(Long.parseLong(randomNumeric(2)), randomAlphabetic(4)));
+        return fooList;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void create(@RequestBody Foo newFoo) {
+        log.info("Foo created");
+    }
+}
---
+++ src/main/java/com/app/warrantychecker/model/Foo.java
@@ -0,0 +1,11 @@
+package com.app.warrantychecker.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Foo {
+    private long id;
+    private String name;
+}
