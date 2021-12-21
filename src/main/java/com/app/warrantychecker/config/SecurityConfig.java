4f72329411fa00cee0360eb31b4bb857779c38d2
Nazri <649000@users.noreply.github.com>
2021-12-21 11:33:32 +0800

    Added SecurityConfig configuration

---
+++ src/main/java/com/app/warrantychecker/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.app.warrantychecker.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+    }
+
+}
