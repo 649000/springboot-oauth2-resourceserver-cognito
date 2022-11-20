54fb42ecb98662f2ce3e5ffc3a92c8fa5e711638
Nazri <649000@users.noreply.github.com>
2022-11-20 23:14:09 +0800

    Added Company and Product repository file

---
+++ src/main/java/com/N649000/warrantytracker/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.N649000.warrantytracker.repository;
+
+import com.N649000.warrantytracker.model.Product;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface ProductRepository extends CrudRepository<Product, String> {
+}
