c808478fff10e6720d81f45e6a11753543e536dc
Nazri <649000@users.noreply.github.com>
2022-11-22 22:45:52 +0800

    Implemented controllers for Company and Product

--- src/main/java/com/N649000/warrantytracker/controller/CompanyController.java
+++ src/main/java/com/N649000/warrantytracker/controller/CompanyController.java
@@ -1,7 +1,56 @@
 package com.N649000.warrantytracker.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.N649000.warrantytracker.model.Company;
+import com.N649000.warrantytracker.service.CompanyService;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "/api/company")
+@Slf4j
 public class CompanyController {
+
+    //TODO: Adding validation
+    //https://stackoverflow.com/questions/67454378/spring-boot-validation-with-spring-boot-starter-validation-is-not-working-for-sp
+    private final CompanyService companyService;
+
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Company createCompany (@RequestBody Company request) {
+        log.debug("Company Object: {}", request.toString());
+        return companyService.createCompany(request);
+    }
+
+    @GetMapping
+    public Iterable<Company> getCompanies(){
+        return companyService.getCompanies();
+    }
+
+    @GetMapping("/{companyId}")
+    public Company getCompany(@PathVariable @NonNull String companyId) {
+        log.debug("Company ID: {}", companyId);
+        if(companyId==null){
+            log.info("YUP ITS NULL");
+        }
+        return companyService.getCompany(companyId);
+    }
+
+    @PatchMapping("/{companyId}")
+    public Company updateCompany (@PathVariable("companyId") String companyId, @RequestBody Company request) {
+        log.debug("Company ID: {}", companyId);
+        log.debug("Company Object: {}", request.toString());
+        return companyService.updateCompany(companyId, request);
+    }
+
+    @DeleteMapping("/{companyId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCompany (@PathVariable String companyId) {
+        log.debug("Company ID: {}", companyId);
+        companyService.deleteCompany(companyId);
+    }
 }
--- src/main/java/com/N649000/warrantytracker/controller/ProductController.java
+++ src/main/java/com/N649000/warrantytracker/controller/ProductController.java
@@ -1,11 +1,54 @@
 package com.N649000.warrantytracker.controller;
 
+import com.N649000.warrantytracker.model.Product;
+import com.N649000.warrantytracker.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/product")
 @Slf4j
 public class ProductController {
+
+    private final ProductService productService;
+
+@Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product createProduct (@RequestBody Product request) {
+        log.debug("Product Object: {}", request.toString());
+        return productService.createProduct(request);
+    }
+
+    @GetMapping
+    public Iterable<Product> getCompanies(){
+        return productService.getProducts();
+    }
+
+    @GetMapping("/{productId}")
+    public Product getProduct(@PathVariable String productId) {
+        log.debug("Product ID: {}", productId);
+        return productService.getProduct(productId);
+    }
+
+    @PatchMapping("/{productId}")
+    public Product updateBook (@PathVariable("productId") String productId, @RequestBody Product request) {
+        log.debug("Product ID: {}", productId);
+        log.debug("Product Object: {}", request.toString());
+        return productService.updateProduct(productId, request);
+    }
+
+    @DeleteMapping("/{productId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBook (@PathVariable @NonNull String productId) {
+        log.debug("Product ID: {}", productId);
+        productService.deleteProduct(productId);
+    }
 }
