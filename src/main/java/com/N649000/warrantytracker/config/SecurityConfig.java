eef94e6dae511c1d946c68610d0be8c6c3ba7e61
Nazri <649000@users.noreply.github.com>
2023-02-16 16:08:59 +0800

    Updated SecurityConfig to use SecurityFilterChain as per latest version of Spring Security

--- src/main/java/com/N649000/warrantytracker/config/SecurityConfig.java
+++ src/main/java/com/N649000/warrantytracker/config/SecurityConfig.java
@@ -1,24 +1,31 @@
 package com.N649000.warrantytracker.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // https://docs.spring.io/spring-security/reference/5.8/migration/servlet/config.html#use-new-requestmatchers
+        // URLs should from most restrictive to the least restrictive
+        return http
+                .securityMatcher("/api/**")
+                .authorizeHttpRequests(authorize -> authorize
+                                .requestMatchers("/api/**").authenticated()
+                                .requestMatchers("/**").permitAll()
+                        // All request will be authenticated
+                        // .anyRequest().authenticated()
+                )
+                .sessionManagement(session ->
+                        // REST API don't need session management
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
-                .and()
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .oauth2ResourceServer()
-                .jwt();
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt())
+                .build();
     }
-
 }
